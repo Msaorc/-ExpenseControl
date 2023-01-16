998e4755e74d836a1d9f374ee3eb4ddeb8bde77d
Marcos Augusto <msaorc@hotmail.com>
2023-01-16 15:29:20 -0300

    Creation of the user db with the tests

---
+++ internal/infra/database/user_db_test.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/Msaorc/ExpenseControlAPI/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file:memory.db"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("Marcos Augusto", "marcos@email.com", "200")
+	userDB := NewUser(db)
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+	var userFinded entity.User
+
+	err = db.First(&userFinded, "id = ?", user.ID).Error
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFinded.ID)
+	assert.Equal(t, user.Name, userFinded.Name)
+	assert.Equal(t, user.Email, userFinded.Email)
+	assert.NotNil(t, userFinded.Password)
+}
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file:memory.db"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("Marcos Augusto", "email@email.com", "200")
+	userDB := NewUser(db)
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+	userFindByEmail, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.NotNil(t, userFindByEmail)
+	assert.Equal(t, user.ID, userFindByEmail.ID)
+	assert.Equal(t, user.Name, userFindByEmail.Name)
+	assert.Equal(t, user.Email, userFindByEmail.Email)
+	assert.NotNil(t, userFindByEmail.Password)
+}
