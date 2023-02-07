2e3e542e91a6cf0becbbc9732e8a6b1cfa7c6172
Marcos Augusto <msaorc@hotmail.com>
2023-02-07 17:33:36 -0300

    Creation of the ExpenseLevelhandler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Msaorc/ExpenseControlAPI/configs"
+	"github.com/Msaorc/ExpenseControlAPI/internal/dto"
+	"github.com/Msaorc/ExpenseControlAPI/internal/entity"
+	"github.com/Msaorc/ExpenseControlAPI/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("Initial Commit from ExepenseControl")
+	_, err := configs.LoadConfigs(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("file:expense.db"))
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.ExpenseOrigin{}, &entity.ExpenseLevel{}, &entity.Expense{})
+	http.ListenAndServe(":8080", nil)
+}
+
+type ExpenseLevelHandler struct {
+	ExpenseLevelDB database.ExpenseLevelInterface
+}
+
+func NewExpenseOriginHandler(db database.ExpenseLevelInterface) *ExpenseLevelHandler {
+	return &ExpenseLevelHandler{
+		ExpenseLevelDB: db,
+	}
+}
+
+func (eo *ExpenseLevelHandler) CreateExpenseLevel(w http.ResponseWriter, r *http.Request) {
+	var expenseLevel dto.ExepnseLevel
+	err := json.NewDecoder(r.Body).Decode(&expenseLevel)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	el, err := entity.NewExpenseLevel(expenseLevel.Description)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = eo.ExpenseLevelDB.Create(el)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
---
+++ internal/dto/dto.go
@@ -0,0 +1,5 @@
+package dto
+
+type ExepnseLevel struct {
+	Description string `json:"description"`
+}
